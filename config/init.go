2da24f75205c87e6ded482f3e9bf636f911c7f1f
wujc <342464096@qq.com>
2022-03-08 14:24:18 +0800

    1、添加部分注释

--- config/init.go
+++ config/init.go
@@ -1,26 +1,28 @@
 package config
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"runtime"
 	"strings"
 )
 
-func init()  {
+func init() {
+	fmt.Println(os.Args[0])
 	execDir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
 	var splitStr = "/"
 	if runtime.GOOS == "windows" {
 		splitStr = `\`
 	}
-	pathArr := strings.Split(execDir,splitStr)
+	pathArr := strings.Split(execDir, splitStr)
 	BifrostDirName := pathArr[len(pathArr)-1]
 	switch BifrostDirName {
-	case "bin","sbin":
-		BifrostDir = filepath.Dir(execDir)+splitStr
+	case "bin", "sbin":
+		BifrostDir = filepath.Dir(execDir) + splitStr
 		break
 	default:
-		BifrostDir = execDir+splitStr
+		BifrostDir = execDir + splitStr
 		break
 	}
 }
